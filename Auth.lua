98ff6a1cfa46453aef9ee0268a8d4abdf45ec5b5
NobodyOffcal <100217070+NobodyOffcal@users.noreply.github.com>
2022-04-03 18:13:03 +0300

    Add files via upload

---
+++ Auth.lua
@@ -0,0 +1,44 @@
+﻿--Simple DataBase
+
+return
+{
+
+    "Leaked_False",
+
+    --Fatee#5605
+
+    "Banned_FalseForFate",
+    "AISD9SAD8FHGU243ASDDYJG4",
+
+    --SusNobody#4877
+
+    "Banned_FalseForNobody",
+    "DSJHREQWDFSGFHYYIOIU129",
+
+    --yourdailydoseofinternet#9491
+
+    "Banned_FalseForyourdailydoseofinternet",
+    "ASDHTRWVCBMJHGOIOULD126",
+
+    --ₘᵢₙᵢ ₛᵤₛ#7530
+
+    "Banned_FalseForMiniSus",
+    "SAD23GFDGTRYUHTJHGIKUYQ",
+
+    --me756j#7490
+
+    "Banned_FalseForme",
+    "DASDSADSFGBVOFDTWER163",
+
+    --Alec <9#1445
+
+    "Banned_TrueForAlec",
+    "BANNED", 
+    "BanReason_TryLeakingScript", 
+
+    --NIGGA LIL BLCK#7984
+
+    "Banned_FalseForNigga", 
+    "DSAYTRUYIFGHHKUIOUIE123AS",
+
+}
